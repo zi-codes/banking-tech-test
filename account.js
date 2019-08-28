b2b57b969e443540fe46837a48be6b5fc5423c0a
Student <ziwei.polaris@gmail.com>
2019-08-28 14:19:36 +0100

    2 classes linted and refactored conditionals to ternaries

--- account.js
+++ account.js
@@ -1,23 +1,22 @@
-import HistoryHandler from './historyHandler'
+import HistoryHandler from "./historyHandler";
 
 export default class Account {
   constructor(historyHandler = new HistoryHandler()) {
-    this.balance = 0
-    this.historyHandler = historyHandler
+    this.balance = 0;
+    this.historyHandler = historyHandler;
   }
 
-  add(amount,date = Date(Date.now())) {
-    this.balance += amount
-    this.historyHandler.logHistory(amount,date,false,this.balance)
+  add(amount, date = Date(Date.now())) {
+    this.balance += amount;
+    this.historyHandler.logHistory(amount, date, false, this.balance);
   }
 
-  withdraw(amount,date = Date(Date.now())) {
-    this.balance -= amount
-    this.historyHandler.logHistory(amount,date,true,this.balance)
+  withdraw(amount, date = Date(Date.now())) {
+    this.balance -= amount;
+    this.historyHandler.logHistory(amount, date, true, this.balance);
   }
 
   printHistory() {
-    return this.historyHandler.print()
+    return this.historyHandler.print();
   }
-
 }
--- historyHandler.js
+++ historyHandler.js
@@ -1,54 +1,48 @@
 export default class HistoryHandler {
   constructor() {
-    this.history = []
+    this.history = [];
   }
 
-  logHistory(amount,date,debit,balance) {
+  logHistory(amount, date, debit, balance) {
     this.history.push({
       date: date,
       debit: debit,
       amount: amount.toFixed(2),
       balance: balance.toFixed(2)
-    })
-
+    });
   }
 
   print() {
-    return this.printHeaders() + this.printEntries()
+    return this.printHeaders() + this.printEntries();
   }
 
   printHeaders() {
-    return("date || credit || debit || balance\n")
+    return "date || credit || debit || balance\n";
   }
 
-  printEntries () {
-    return this.history.reverse().map( entry =>
-      this.printEntry(entry)
-    ).join("\n")
+  printEntries() {
+    return this.history
+      .reverse()
+      .map(entry => this.printEntry(entry))
+      .join("\n");
   }
 
-  printEntry (entry) {
-    return `${this.parseDate(entry.date)} || ${this.calculateCredit(entry)} || ${this.calculateDebit(entry)} || ${entry.balance}`
+  printEntry(entry) {
+    return `${this.parseDate(entry.date)} || ${this.calculateCredit(
+      entry
+    )} || ${this.calculateDebit(entry)} || ${entry.balance}`;
   }
 
-  parseDate (date) {
-    let moment = require('moment')
-    return moment(date).format("DD/MM/YYYY")
+  parseDate(date) {
+    let moment = require("moment");
+    return moment(date).format("DD/MM/YYYY");
   }
 
-  calculateDebit (entry) {
-    if (entry.debit === true) {
-      return `${entry.amount}`
-    } else {
-      return ""
-    }
+  calculateDebit(entry) {
+    return entry.debit ? `${entry.amount}` : "";
   }
 
-  calculateCredit (entry) {
-    if (entry.debit === false) {
-      return `${entry.amount}`
-    } else {
-      return ""
-    }
+  calculateCredit(entry) {
+    return entry.debit ? "" : `${entry.amount}`;
   }
 }
